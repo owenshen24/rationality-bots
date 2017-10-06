68ef5abdc4206563a1141eb440b687bb090e8c3c
Owen Shen <owenshen24@gmail.com>
2017-10-06 15:47:05 -0700

    Added new tdl bot.

--- scripts/checkbot.js
+++ scripts/checkbot.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
 function createToDo() {
   var toDo = $(".enter-form").val();
   var blank = "";
-  $(".to-dos").append("<div class='active-to-do big-text'>" + toDo + "</div>");
+  $(".to-dos").append("<div class='active to-do big-text'>" + toDo + "</div>");
   $(".enter-form").val(blank);
 }
 $(document).keypress(function(e) {
@@ -14,9 +14,31 @@ $(document).keypress(function(e) {
     }
 });
 
-function strikeThrough() {
-  
+//Allows the stuff to be crossed out.
+$(".to-dos").click(function(e) {
+  let li = e.target;
+
+  if (li.style.textDecoration === "none") {
+  li.style.textDecoration = "line-through";
+  li.style.opacity = "0.25";
+  }
+  else {
+      li.style.textDecoration = "none";
+      li.style.opacity = "1.0";
+  }
+});
+
+
+function removeToDo() {
+  $(".to-do").remove();
 }
+$(".reset-button").click(removeToDo);
+// Alternative CTRL-x can be used to remove.
+$(document).on('keydown', function ( e ) {
+    if ((e.metaKey || e.ctrlKey) && ( String.fromCharCode(e.which).toLowerCase() === 'x') ) {
+        removeToDo();
+    }
+});
 
 
 });
